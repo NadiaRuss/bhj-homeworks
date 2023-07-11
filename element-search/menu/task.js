b05d5b12afd40fb56412d4ec85562ed3bf91f135
Надежда Яковлева <i160220@mail.ru>
2023-07-11 13:32:41 +0700

    Задача решена

---
+++ element-search/menu/task.js
@@ -0,0 +1,10 @@
+let menuLink = document.querySelectorAll('.menu__link');
+for(let link of menuLink){
+    link.onclick = function(e){
+        const menuSub = link.parentElement.querySelector('.menu_sub');
+        if(menuSub) {
+            e.preventDefault();
+            menuSub.classList.toggle('menu_active');
+        }
+    }
+}
