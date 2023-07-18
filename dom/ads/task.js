6aebddeb47d486e3815f9dcf0dbb677e3b9e74c0
Надежда Яковлева <i160220@mail.ru>
2023-07-18 15:48:02 +0700

    error

---
+++ dom/ads/task.js
@@ -0,0 +1,14 @@
+let rotatorCase = Array.from(document.querySelectorAll('.rotator__case'));
+let i = rotatorCase.findIndex(n => n.classList.contains('rotator__case_active'));
+
+let interval = setInterval(() => {
+    rotatorCase[i].classList.remove('rotator__case_active');
+    i++
+
+    if(i === rotatorCase.length){
+        i = 0;
+    }
+    rotatorCase[i].style.color = rotatorCase[i].dataset.color
+    rotatorCase[i].classList.add('rotator__case_active');
+
+}, 1000);
---
+++ dom/book-reader/task.js
@@ -0,0 +1,85 @@
+let fontSizeLink = Array.from(document.querySelectorAll('.font-size'));
+let bookColor = document.querySelectorAll('.color')
+let book = document.querySelector('.book')
+fontSizeLink.forEach(function(elem) {
+    elem.addEventListener('click', function(i) {
+        i.preventDefault();
+		fontSizeLink.forEach(el => {
+			if (el != this) {
+				el.classList.remove('font-size_active');
+			}
+        })
+        this.classList.add('font-size_active');
+        let size = elem.dataset.size;
+        if(size === "small"){
+            book.classList.remove('book_fs-big');
+            book.classList.add('book_fs-small');
+        } else if (size === "big"){
+            book.classList.remove('book_fs-small');
+            book.classList.add('book_fs-big');
+        } else{
+            book.classList.remove('book_fs-small');
+            book.classList.remove('book_fs-big');
+        }
+    })
+})
+let colorLink = document.querySelectorAll('.text_color_black, .text_color_gray, .text_color_whitesmoke');
+let bgLink = document.querySelectorAll('.bg_color_black, .bg_color_gray, .bg_color_white')
+
+colorLink.forEach(function(elem) {
+    elem.addEventListener('click', function(i) {
+        i.preventDefault();
+		colorLink.forEach(el => {
+			if (el != this) {
+				el.classList.remove('color_active');
+			}
+        })
+        this.classList.add('color_active');
+
+        let color = elem.dataset.textColor;
+        book.style.color = color;
+
+        // // если нужно именно добавлять класс для book, тогда так:
+
+        // if(color === "gray"){
+        //     book.classList.remove('book_color-black');
+        //     book.classList.remove('book_color-whitesmoke');
+        //     book.classList.add('book_color-gray');
+        // } else if (color === "whitesmoke"){
+        //     book.classList.remove('book_color-black');
+        //     book.classList.remove('book_color-gray');
+        //     book.classList.add('book_color-whitesmoke');
+
+        // } else{
+        //     book.classList.remove('book_color-gray');
+        //     book.classList.remove('book_color-whitesmoke');
+        // }
+    })
+})
+bgLink.forEach(function(elem) {
+    elem.addEventListener('click', function(i) {
+        i.preventDefault();
+		bgLink.forEach(el => {
+			if (el != this) {
+				el.classList.remove('color_active');
+			}
+        })
+        this.classList.add('color_active');
+
+        let bgColor = elem.dataset.bgColor;
+        book.style.backgroundColor = bgColor;
+        // if(bgColor === "gray"){
+        //     book.classList.remove('book_bg-black');
+        //     book.classList.remove('book_bg-white');
+        //     book.classList.add('book_bg-gray');
+        // } else if (bgColor === "black"){
+        //     book.classList.remove('book_bg-white');
+        //     book.classList.remove('book_bg-gray');
+        //     book.classList.add('book_bg-black');
+
+        // } else{
+        //     book.classList.remove('book_bg-gray');
+        //     book.classList.remove('book_bg-black');
+        // }
+    })
+})
